f5fda7b01f3cc3d3258597414aec3ec488e0eb9a
kaitlynclements <123575277+kaitlynclements@users.noreply.github.com>
2024-10-21 21:37:27 -0500

    Update app.py

--- TravelMate/backend/app.py
+++ TravelMate/backend/app.py
@@ -1,3 +1,10 @@
+"""
+Author: Kaitlyn Clements
+Date: 10/21/2024
+Other Sources: Chat GPT
+Description: Entry point of the Flask application
+"""
+
 from flask import Flask
 from controllers.user_controller import user_bp
 from controllers.trip_controller import trip_bp
