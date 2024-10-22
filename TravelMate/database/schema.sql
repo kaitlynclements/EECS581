4631289771bcbd9640b5eafb1063b6185b664b85
kaitlynclements <123575277+kaitlynclements@users.noreply.github.com>
2024-10-21 21:19:40 -0500

    Create schema.sql

---
+++ TravelMate/database/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE trips (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    destination TEXT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
