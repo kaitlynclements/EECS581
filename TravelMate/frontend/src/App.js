254acb6d3810fb093501ed7e942e18c1ef26008d
kaitlynclements <123575277+kaitlynclements@users.noreply.github.com>
2024-10-21 20:12:17 -0500

    Create App.js

---
+++ TravelMate/frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+import TripList from './components/TripList';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/trips" component={TripList} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
