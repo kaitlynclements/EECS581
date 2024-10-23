d500088dbcaf2286f905fb44e89dd4b619da3fae
Elizabeth (Lizzie) Soltis <soltis.el@gmail.com>
2024-10-23 13:33:29 -0500

    Add files via upload

--- TravelMate/frontend/src/App.js
+++ TravelMate/frontend/src/App.js
@@ -7,19 +7,20 @@ Description: The main React component that acts as the base for other components
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';  // Import the CSS file
-import Register from './components/Register';
+import Navbar from './components/Navbar';
 import Login from './components/Login';
+import Register from './components/Register';
 import TripManager from './components/TripManager';
 
 function App() {
   return (
     <Router>
       <div>
-        <h1>TravelMate</h1>
+        <Navbar />
         <Switch>
-          <Route path="/register" component={Register} />
+          <Route exact path="/" component={() => <h1>Welcome to TravelMate</h1>} />
           <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
           <Route path="/trips" component={TripManager} />
         </Switch>
       </div>
